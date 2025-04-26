6f4303226fa65cd79c42bcf0412565edf5855002
NDERI007 <tnderi373@gmail.com>
2025-04-26 17:34:45 +0300

    added event listener for the gameboard

--- main.js
+++ main.js
@@ -15,15 +15,39 @@
 //human vs human first, easy AI then hard Ai
 const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
-    console.log("form submitted");
     //prevent page refresh
     e.preventDefault();
-    console.log("FormData:", Array.from(new FormData(e.target)));
 
     //initialize user form data
     const formData = new FormData (e.target);
     const data = Object.fromEntries(formData);
     document.querySelector(".modal-wrapper").setAttribute("hidden", true);
-    console.log(data);
+    StartGame(data);
+});
 
-})
+const initializeVar = (data) => {
+    data.choice = +data.choice;
+    data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    data.player1 = "X";
+    data.player2 = "O";
+    data.round = 0;
+    data.currentPlayer = "X";
+    data.gameover = false;
+}
+
+const EventForGameBoard = (data) => {
+    document.querySelectorAll(".box").forEach(box => {
+        box.addEventListener("click",(Event) => {
+            playMove(Event.target,data)
+        })
+    })
+}
+const StartGame = (data) => {
+    //initialize game variable
+    //add event listners to the game
+    EventForGameBoard(data);
+    initializeVar(data);
+}
+const playMove = (box, data) => {
+    console.log(box, data);
+}
